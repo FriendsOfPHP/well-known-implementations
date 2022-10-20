OskarStark/generic-implementations

Implement runtime auto-discovery

src/Internal/Guzzle/GuzzlePsr7Request.php
@@ -1,0 +1,29 @@
+<?php
+
+/*
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FriendsOfPHP\GenericImplementations\Internal\Guzzle;
+
+use GuzzleHttp\Psr7\Request;
+
+class_alias(Request::class, GuzzlePsr7Request::class);
+
+if (false) {
+    /**
+     * @internal
+     */
+    class GuzzlePsr7Request extends Request
+    {
+        /**
+         * @param UriInterface|string $uri
+         */
+        public function __construct(string $method, $uri)
+        {
+        }
+    }
+}

src/Internal/Guzzle/GuzzlePsr7Uri.php
@@ -1,0 +1,26 @@
+<?php
+
+/*
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FriendsOfPHP\GenericImplementations\Internal\Guzzle;
+
+use GuzzleHttp\Psr7\Uri;
+
+class_alias(Uri::class, GuzzlePsr7Uri::class);
+
+if (false) {
+    /**
+     * @internal
+     */
+    class GuzzlePsr7Uri extends Uri
+    {
+        public function __construct(string $uri = '')
+        {
+        }
+    }
+}

src/Internal/Nyholm/NyholmPsr7Request.php
@@ -1,0 +1,29 @@
+<?php
+
+/*
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FriendsOfPHP\GenericImplementations\Internal\Nyholm;
+
+use Nyholm\Psr7\Request;
+
+class_alias(Request::class, NyholmPsr7Request::class);
+
+if (false) {
+    /**
+     * @internal
+     */
+    class NyholmPsr7Request extends Request
+    {
+        /**
+         * @param UriInterface|string $uri
+         */
+        public function __construct(string $method, $uri)
+        {
+        }
+    }
+}

src/Internal/Psr7StreamHelper.php
@@ -1,0 +1,50 @@
+<?php
+
+/*
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FriendsOfPHP\GenericImplementations\Internal;
+
+/**
+ * @internal
+ */
+class Psr7StreamHelper
+{
+    /**
+     * @param string|resource $content
+     *
+     * @return resource
+     */
+    public static function toResource($content, ?string $mode)
+    {
+        if (\is_resource($content)) {
+            return $content;
+        }
+
+        if (null === $mode) {
+            $h = fopen('php://temp', 'r+');
+            fwrite($h, $content);
+            rewind($h);
+
+            return $h;
+        }
+
+        if ('' === $content) {
+            throw new \RuntimeException('Path cannot be empty');
+        }
+
+        if (false === $h = @fopen($content, $mode)) {
+            if ('' === $mode || false === \in_array($mode[0], ['r', 'w', 'a', 'x', 'c'], true)) {
+                throw new \InvalidArgumentException(sprintf('The mode "%s" is invalid.', $mode));
+            }
+
+            throw new \RuntimeException(sprintf('The file "%s" cannot be opened: %s', $content, error_get_last()['message'] ?? ''));
+        }
+
+        return $h;
+    }
+}

src/Internal/Slim/SlimPsr7Request.php
@@ -1,0 +1,27 @@
+<?php
+
+/*
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FriendsOfPHP\GenericImplementations\Internal\Slim;
+
+use Slim\Psr7\Headers;
+use Slim\Psr7\Request;
+
+/**
+ * @internal
+ */
+class SlimPsr7Request extends Request
+{
+    /**
+     * @param UriInterface|string $uri
+     */
+    public function __construct(string $method, $uri)
+    {
+        parent::__construct($method, \is_string($uri) ? new SlimPsr7Uri($uri) : $uri, new Headers(), [], [], new SlimPsr7Stream());
+    }
+}

src/Internal/Slim/SlimPsr7Response.php
@@ -1,0 +1,33 @@
+<?php
+
+/*
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FriendsOfPHP\GenericImplementations\Internal\Slim;
+
+use Slim\Psr7\Response;
+
+/**
+ * @internal
+ */
+class SlimPsr7Response extends Response
+{
+    public function __construct(int $code = 200, string $reasonPhrase = '')
+    {
+        parent::__construct($code);
+
+        if ('' !== $reasonPhrase) {
+            static $sync;
+
+            $sync = $sync ?? \Closure::bind(static function ($response, $reasonPhrase) {
+                $response->reasonPhrase = $response->filterReasonPhrase($reasonPhrase);
+            }, null, parent::class);
+
+            $sync($this, $reasonPhrase);
+        }
+    }
+}

src/Internal/Slim/SlimPsr7UploadedFile.php
@@ -1,0 +1,62 @@
+<?php
+
+/*
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FriendsOfPHP\GenericImplementations\Internal\Slim;
+
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UploadedFileInterface;
+use Slim\Psr7\Factory\UploadedFileFactory;
+
+/**
+ * @internal
+ */
+class SlimPsr7UploadedFile implements UploadedFileInterface
+{
+    private $file;
+
+    public function __construct(StreamInterface $stream, int $size = null, int $error = \UPLOAD_ERR_OK, string $clientFilename = null, string $clientMediaType = null)
+    {
+        $this->file = (new UploadedFileFactory())->createUploadedFile(...\func_get_args());
+    }
+
+    public function getStream()
+    {
+        return $this->file->getStream();
+    }
+
+    public function moveTo($targetPath): void
+    {
+        $this->file->moveTo($targetPath);
+    }
+
+    public function getError(): int
+    {
+        return $this->file->getError();
+    }
+
+    public function getClientFilename(): ?string
+    {
+        return $this->file->getClientFilename();
+    }
+
+    public function getClientMediaType(): ?string
+    {
+        return $this->file->getClientMediaType();
+    }
+
+    public function getSize(): ?int
+    {
+        return $this->file->getSize();
+    }
+
+    public function getFilePath(): string
+    {
+        return $this->getFilePath();
+    }
+}

src/Internal/Symfony/SymfonyPsr18Client.php
@@ -1,0 +1,34 @@
+<?php
+
+/*
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FriendsOfPHP\GenericImplementations\Internal\Symfony;
+
+use FriendsOfPHP\GenericImplementations\GenericPsr17Factory;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Symfony\Component\HttpClient\Psr18Client;
+
+/**
+ * @internal
+ */
+class SymfonyPsr18Client implements ClientInterface
+{
+    private $client;
+
+    public function __construct()
+    {
+        $this->client = new Psr18Client(null, new GenericPsr17Factory());
+    }
+
+    public function sendRequest(RequestInterface $request): ResponseInterface
+    {
+        return $this->client->sendRequest($request);
+    }
+}

FriendsOfPHP/well-known-implementations

src/Internal/Guzzle/GuzzlePsr7Request.php
@@ -1,0 +1,29 @@
+<?php
+
+/*
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FriendsOfPHP\WellKnownImplementations\Internal\Guzzle;
+
+use GuzzleHttp\Psr7\Request;
+
+class_alias(Request::class, GuzzlePsr7Request::class);
+
+if (false) {
+    /**
+     * @internal
+     */
+    class GuzzlePsr7Request extends Request
+    {
+        /**
+         * @param UriInterface|string $uri
+         */
+        public function __construct(string $method, $uri)
+        {
+        }
+    }
+}

src/Internal/Guzzle/GuzzlePsr7Uri.php
@@ -1,0 +1,26 @@
+<?php
+
+/*
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FriendsOfPHP\WellKnownImplementations\Internal\Guzzle;
+
+use GuzzleHttp\Psr7\Uri;
+
+class_alias(Uri::class, GuzzlePsr7Uri::class);
+
+if (false) {
+    /**
+     * @internal
+     */
+    class GuzzlePsr7Uri extends Uri
+    {
+        public function __construct(string $uri = '')
+        {
+        }
+    }
+}

src/Internal/Nyholm/NyholmPsr7Request.php
@@ -1,0 +1,29 @@
+<?php
+
+/*
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FriendsOfPHP\WellKnownImplementations\Internal\Nyholm;
+
+use Nyholm\Psr7\Request;
+
+class_alias(Request::class, NyholmPsr7Request::class);
+
+if (false) {
+    /**
+     * @internal
+     */
+    class NyholmPsr7Request extends Request
+    {
+        /**
+         * @param UriInterface|string $uri
+         */
+        public function __construct(string $method, $uri)
+        {
+        }
+    }
+}

src/Internal/Psr7StreamHelper.php
@@ -1,0 +1,50 @@
+<?php
+
+/*
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FriendsOfPHP\WellKnownImplementations\Internal;
+
+/**
+ * @internal
+ */
+class Psr7StreamHelper
+{
+    /**
+     * @param string|resource $content
+     *
+     * @return resource
+     */
+    public static function toResource($content, ?string $mode)
+    {
+        if (\is_resource($content)) {
+            return $content;
+        }
+
+        if (null === $mode) {
+            $h = fopen('php://temp', 'r+');
+            fwrite($h, $content);
+            rewind($h);
+
+            return $h;
+        }
+
+        if ('' === $content) {
+            throw new \RuntimeException('Path cannot be empty');
+        }
+
+        if (false === $h = @fopen($content, $mode)) {
+            if ('' === $mode || false === \in_array($mode[0], ['r', 'w', 'a', 'x', 'c'], true)) {
+                throw new \InvalidArgumentException(sprintf('The mode "%s" is invalid.', $mode));
+            }
+
+            throw new \RuntimeException(sprintf('The file "%s" cannot be opened: %s', $content, error_get_last()['message'] ?? ''));
+        }
+
+        return $h;
+    }
+}

src/Internal/Slim/SlimPsr7Request.php
@@ -1,0 +1,27 @@
+<?php
+
+/*
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FriendsOfPHP\WellKnownImplementations\Internal\Slim;
+
+use Slim\Psr7\Headers;
+use Slim\Psr7\Request;
+
+/**
+ * @internal
+ */
+class SlimPsr7Request extends Request
+{
+    /**
+     * @param UriInterface|string $uri
+     */
+    public function __construct(string $method, $uri)
+    {
+        parent::__construct($method, \is_string($uri) ? new SlimPsr7Uri($uri) : $uri, new Headers(), [], [], new SlimPsr7Stream());
+    }
+}

src/Internal/Slim/SlimPsr7Response.php
@@ -1,0 +1,33 @@
+<?php
+
+/*
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FriendsOfPHP\WellKnownImplementations\Internal\Slim;
+
+use Slim\Psr7\Response;
+
+/**
+ * @internal
+ */
+class SlimPsr7Response extends Response
+{
+    public function __construct(int $code = 200, string $reasonPhrase = '')
+    {
+        parent::__construct($code);
+
+        if ('' !== $reasonPhrase) {
+            static $sync;
+
+            $sync = $sync ?? \Closure::bind(static function ($response, $reasonPhrase) {
+                $response->reasonPhrase = $response->filterReasonPhrase($reasonPhrase);
+            }, null, parent::class);
+
+            $sync($this, $reasonPhrase);
+        }
+    }
+}

src/Internal/Slim/SlimPsr7UploadedFile.php
@@ -1,0 +1,62 @@
+<?php
+
+/*
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FriendsOfPHP\WellKnownImplementations\Internal\Slim;
+
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UploadedFileInterface;
+use Slim\Psr7\Factory\UploadedFileFactory;
+
+/**
+ * @internal
+ */
+class SlimPsr7UploadedFile implements UploadedFileInterface
+{
+    private $file;
+
+    public function __construct(StreamInterface $stream, int $size = null, int $error = \UPLOAD_ERR_OK, string $clientFilename = null, string $clientMediaType = null)
+    {
+        $this->file = (new UploadedFileFactory())->createUploadedFile(...\func_get_args());
+    }
+
+    public function getStream()
+    {
+        return $this->file->getStream();
+    }
+
+    public function moveTo($targetPath): void
+    {
+        $this->file->moveTo($targetPath);
+    }
+
+    public function getError(): int
+    {
+        return $this->file->getError();
+    }
+
+    public function getClientFilename(): ?string
+    {
+        return $this->file->getClientFilename();
+    }
+
+    public function getClientMediaType(): ?string
+    {
+        return $this->file->getClientMediaType();
+    }
+
+    public function getSize(): ?int
+    {
+        return $this->file->getSize();
+    }
+
+    public function getFilePath(): string
+    {
+        return $this->getFilePath();
+    }
+}

src/Internal/Symfony/SymfonyPsr18Client.php
@@ -1,0 +1,34 @@
+<?php
+
+/*
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FriendsOfPHP\WellKnownImplementations\Internal\Symfony;
+
+use FriendsOfPHP\WellKnownImplementations\WellKnownPsr17Factory;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Symfony\Component\HttpClient\Psr18Client;
+
+/**
+ * @internal
+ */
+class SymfonyPsr18Client implements ClientInterface
+{
+    private $client;
+
+    public function __construct()
+    {
+        $this->client = new Psr18Client(null, new WellKnownPsr17Factory());
+    }
+
+    public function sendRequest(RequestInterface $request): ResponseInterface
+    {
+        return $this->client->sendRequest($request);
+    }
+}