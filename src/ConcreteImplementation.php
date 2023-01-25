b8bcd6f9689961fca73456b17ec945dbf7c433f9
Nicolas Grekas <nicolas.grekas@gmail.com>
2023-01-25 18:51:04 +0100

    Rely on php-http/discovery v1.15

--- src/ConcreteImplementation.php
+++ src/ConcreteImplementation.php
@@ -9,12 +9,14 @@
 
 namespace FriendsOfPHP\WellKnownImplementations;
 
-use Composer\InstalledVersions;
+use Http\Client\HttpAsyncClient;
+use Http\Client\HttpClient;
+use Http\Discovery\ClassDiscovery;
 
 /**
  * @author Nicolas Grekas <p@tchwork.com>
  */
-class ConcreteImplementation
+class ConcreteImplementation extends ClassDiscovery
 {
     public const PSR7_VENDOR = PSR7_VENDOR;
     public const PSR18_VENDOR = PSR18_VENDOR;
@@ -29,29 +31,40 @@ class ConcreteImplementation
     public const VENDOR_LAMINAS = 'Laminas';
     public const VENDOR_NYHOLM = 'Nyholm';
     public const VENDOR_SLIM = 'Slim';
+    public const VENDOR_SLIM3 = 'Slim3';
     public const VENDOR_SYMFONY = 'Symfony';
 
-    /**
-     * @internal
-     */
-    public const IMPLEMENTATIONS = [
+    private const IMPLEMENTATIONS = [
         'PSR7' => [
-            'Nyholm' => 'nyholm/psr7',
-            'Guzzle' => ['guzzlehttp/psr7' => '1.4'],
-            'Slim' => 'slim/psr7',
-            'Laminas' => 'laminas/laminas-diactoros',
+            // Phalcon
+            // Diactoros
+            // Slim3
+            'Nyholm' => \Nyholm\Psr7\Request::class,
+            'Guzzle' => \GuzzleHttp\Psr7\Request::class,
+            'Slim' => \Slim\Psr7\Request::class,
+            'Laminas' => \Laminas\Diactoros\Request::class,
         ],
         'PSR18' => [
-            'Symfony' => 'symfony/http-client',
-            'Guzzle' => ['guzzlehttp/guzzle' => '7.0'],
+            'Symfony' => \Symfony\Component\HttpClient\Psr18Client::class,
+            'Guzzle' => \GuzzleHttp\Client::class,
+            // buzz
         ],
         'HTTPLUG' => [
-            'Symfony' => ['symfony/http-client' => '4.4'],
-            'HttplugGuzzle7' => 'php-http/guzzle7-adapter',
-            'HttplugGuzzle6' => 'php-http/guzzle6-adapter',
-            'HttplugCurl' => 'php-http/curl-client',
-            'HttplugReact' => 'php-http/react-adapter',
-            'HttplugMock' => 'php-http/mock-client',
+            'Symfony' => \Symfony\Component\HttpClient\HttplugClient::class,
+            'HttplugGuzzle7' => \Http\Adapter\Guzzle7\Client::class,
+            'HttplugGuzzle6' => \Http\Adapter\Guzzle6\Client::class,
+            // Guzzle 5
+            'HttplugCurl' => \Http\Client\Curl\Client::class,
+            'HttplugReact' => \Http\Adapter\React\Client::class,
+            'HttplugMock' => \Http\Mock\Client::class,
+
+            //['class' => Socket::class, 'condition' => Socket::class],
+            //['class' => Buzz::class, 'condition' => Buzz::class],
+            //['class' => Cake::class, 'condition' => Cake::class],
+            //['class' => Zend::class, 'condition' => Zend::class],
+            //['class' => Artax::class, 'condition' => Artax::class],
+            //Buzz\Client\FileGetContents
+
         ],
     ];
 
@@ -60,32 +73,16 @@ class ConcreteImplementation
      */
     public static function initialize(): void
     {
-        foreach (self::IMPLEMENTATIONS as $const => $packages) {
+        foreach (self::IMPLEMENTATIONS as $const => $vendors) {
             if (\defined(__NAMESPACE__."\\{$const}_VENDOR")) {
                 continue;
             }
-            foreach ([false, true] as $includeDevRequirements) {
-                foreach ($packages as $vendor => $versions) {
-                    if ('HttplugMock' === $vendor && !$includeDevRequirements) {
-                        continue;
-                    }
-                    foreach ((array) $versions as $package => $version) {
-                        if (\is_int($package)) {
-                            $package = $version;
-                            $version = null;
-                        }
-                        if (!InstalledVersions::isInstalled($package, $includeDevRequirements)) {
-                            continue 2;
-                        }
-                        if (null !== $version && version_compare($installed = InstalledVersions::getVersion($package) ?? $version, $version, '<') && 0 !== strpos($installed, 'dev-')) {
-                            continue 2;
-                        }
-                    }
-                    \define(__NAMESPACE__."\\{$const}_VENDOR", $vendor);
-                    continue 3;
+            foreach ($vendors as $vendor => $class) {
+                if (!class_exists($class)) {
+                    $vendor = null;
                 }
+                \define(__NAMESPACE__."\\{$const}_VENDOR", $vendor);
             }
-            \define(__NAMESPACE__."\\{$const}_VENDOR", null);
         }
     }
 }
